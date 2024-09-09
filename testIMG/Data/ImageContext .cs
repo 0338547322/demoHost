3bf632258eb9a59c604d3a4527bc4ce7eeb6af48
0338547322 <hungtnps28000@fpt.edu.vn>
2024-09-10 03:12:06 +0700

    ok

---
+++ testIMG/Data/ImageContext .cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using testIMG.Models;
+namespace testIMG.Data
+{
+    public class ImageContext : DbContext
+    {
+        public ImageContext(DbContextOptions<ImageContext> options) : base(options) { }
+
+        public DbSet<UploadImageModel> Images { get; set; }
+    }
+}
